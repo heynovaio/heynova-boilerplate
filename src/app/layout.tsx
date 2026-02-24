5fa89f7e888a07cc395a00f0ba354d6ffed66597
Karlee Milbury <karleemilbury@gmail.com>
2026-02-24 11:16:26 -0400

    style: setting up color vars and added brand setting vars

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,52 @@
 import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+import { repositoryName, createClient } from "@/prismicio";
 
 import "./globals.css";
+import React from "react";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const client = createClient();
+
+  const theme = await client.getSingle("colors");
+
+  // Colors
+  const primary = theme.data.primary;
+  const secondary = theme.data.secondary;
+  const highlight = theme.data.highlight;
+  const complimentary = theme.data.complimentary;
+  const black = theme.data.black;
+  const white = theme.data.white;
+  const hyperlink = theme.data.hyperlink;
+  const hover = theme.data.hover;
+  const focus = theme.data.focus;
+
+  // Brand Settings
+  const base_constrast_mode = theme.data.base_contrast_mode;
+  const radius_scale = theme.data.radius_scale;
+  const shadow_style = theme.data.shadow_style;
+
   return (
     <html lang="en">
-      <head>
-        <link rel="stylesheet" href="https://use.typekit.net/sty6ouh.css" />
-      </head>
-      <body>
+      <body
+        style={
+          {
+            "--color-primary": primary,
+            "--color-secondary": secondary,
+            "--color-highlight": highlight,
+            "--color-complimentary": complimentary,
+            "--color-black": black,
+            "--color-white": white,
+            "--color-hyperlink": hyperlink,
+            "--color-hover": hover,
+            "--color-focus": focus,
+          } as React.CSSProperties
+        }
+      >
         <ReactQueryProvider>{children}</ReactQueryProvider>
         <PrismicPreview repositoryName={repositoryName} />
       </body>
