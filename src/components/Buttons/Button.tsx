6deafa949ea680befd7fd45ad4429c3320646535
Karlee Milbury <karleemilbury@gmail.com>
2026-02-27 15:11:25 -0400

    feat: added buttons component and adjusted card component

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,14 @@
+import { LinkField } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+
+interface ButtonProps {
+  button: LinkField;
+  buttonClass?: "primary" | "secondary" | "outline" | "text";
+}
+export const Button = ({ button, buttonClass = "primary" }: ButtonProps) => {
+  return (
+    <PrismicNextLink field={button} className={`btn btn-${buttonClass}`}>
+      {button.text || "Learn more"}
+    </PrismicNextLink>
+  );
+};
