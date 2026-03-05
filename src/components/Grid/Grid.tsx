ffc1fec06445cf37d4aee33ebe36dfeef97f23c3
Karlee Milbury <karleemilbury@gmail.com>
2026-03-05 13:18:27 -0400

    fix: fixed the select for 4 columns not working

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -23,7 +23,7 @@ export const Grid: React.FC<GridProps> = ({
         : columns === 3
           ? "grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3"
           : columns === 4
-            ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+            ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 "
             : "grid-cols-1",
   };
 
